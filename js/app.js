a41beeaf04eace42e0dd6bf1e41566da4b60bda2
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2019-06-10 19:28:00 -0500

    WIP

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
+// Request('https://darksky.net/widget/graph-bar/41.919,-87.7067/uk212/en.js?width=100%&height=400&title=Full Forecast&textColor=ffffff&bgColor=fb0505&transparency=true&skyColor=undefined&fontFamily=Default&customFont=&units=uk2&timeColor=ffffff&tempColor=000000&currentDetailsOption=true')
 
 function checkTime(i) {
     if (i < 10) {
-      i = "0" + i;
+        i = "0" + i;
     }
     return i;
-  }
-  
-  function startTime() {
+}
+
+function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
@@ -14,7 +15,7 @@ function checkTime(i) {
     m = checkTime(m);
     document.getElementById('time').innerHTML = h + ":" + m;
     t = setTimeout(function() {
-      startTime()
+        startTime()
     }, 500);
-  }
-  startTime()
+}
+startTime()
