db7fd3e71df27ec0f0078a9e8d0ce34b15cce968
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2019-06-15 01:49:31 -0500

    Modified request to avoid getting unnecessary data

--- js/api.js
+++ js/api.js
@@ -3,10 +3,12 @@ var config = {
     corsProxy: 'https://cors-anywhere.herokuapp.com/',
     darkSkyURL: 'https://api.darksky.net/forecast/',
     apiKey : '23de50799409973690cd8f7ac2274050',
-    locationCoords: '41.919032,-87.706850'
+    locationCoords: '41.919032,-87.706850',
+    excludeList: "&exclude=[minutely,hourly,daily,alerts,flags]"
 }
 var url = config.corsProxy
 + config.darkSkyURL
 + config.apiKey
 + '/'
-+ config.locationCoords;
++ config.locationCoords + '?'
++ config.excludeList;
